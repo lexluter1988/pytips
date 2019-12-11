14a0fdb64fa7513844ea399fc3dd1dc22e946029
Alexey Suponin <Alexey.Suponin@virtuozzo.com>
2019-12-11 18:52:10 +0700

    closes #39

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from logging.handlers import SMTPHandler
 
 from flask import Flask, current_app
 from flask import request
+from flask_admin.contrib.sqla import ModelView
 from flask_babel import Babel
 from flask_babel import lazy_gettext as _l
 from flask_login import LoginManager
@@ -13,7 +14,9 @@ from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+
 from config import Config
+from flask_admin import Admin
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -25,10 +28,14 @@ bootstrap = Bootstrap()
 moment = Moment()
 babel = Babel()
 
+admin = Admin(name='pytips', template_mode='bootstrap3')
+
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 
     db.init_app(app)
     migrate.init_app(app, db)
@@ -37,6 +44,14 @@ def create_app(config_class=Config):
     bootstrap.init_app(app)
     moment.init_app(app)
     babel.init_app(app)
+
+    from app.models import User, Tip, HashTag, Like, Message
+    admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Tip, db.session))
+    admin.add_view(ModelView(HashTag, db.session))
+    admin.add_view(ModelView(Like, db.session))
+    admin.add_view(ModelView(Message, db.session))
+    admin.init_app(app)
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
