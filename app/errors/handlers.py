7b853b036aa35b3f6ed58f5174353e4cc80909fe
Alexey Suponin <Alexey.Suponin@virtuozzo.com>
2019-12-17 15:44:13 +0700

    fixed 404 500 error custom handlers

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -3,12 +3,12 @@ from app.errors import bp
 from app import db
 
 
-@bp.errorhandler(404)
+@bp.app_errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html'), 404
 
 
-@bp.errorhandler(500)
+@bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('errors/500.html'), 500
