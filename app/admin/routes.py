e4a7c61031ee7d897414a6f9ca29da25938b2cb8
Alexey Suponin <Alexey.Suponin@virtuozzo.com>
2020-01-23 18:23:33 +0700

    closes #93

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -3,8 +3,9 @@ import logging
 from flask import render_template
 from flask_login import login_required
 
+from app import db
 from app.admin import bp
-from app.models import Permissions
+from app.models import Permissions, User
 from app.utils.decorators import check_confirmed, permissions_required
 
 LOG = logging.getLogger(__name__)
@@ -15,4 +16,5 @@ LOG = logging.getLogger(__name__)
 @check_confirmed
 @permissions_required(Permissions.ADMINISTER)
 def admin():
-    return render_template('admin/admin.html')
+    users = db.session.query(User).all()
+    return render_template('admin/admin.html', users=users)
