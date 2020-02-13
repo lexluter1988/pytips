52dfa0525a9b1f596c290f6acdd78f50fc045a30
Alexey Suponin <Alexey.Suponin@virtuozzo.com>
2020-02-13 23:35:23 +0700

    closes #108

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,57 +1,78 @@
+import os
+
 import pytest
 
 from app import create_app, db
-from app.models import User
+from app.models import User, Tip
+from config import basedir
+
+
+def login(client, username, password):
+    return client.post('/login', data=dict(
+        username=username,
+        password=password
+    ), follow_redirects=True)
+
+
+def logout(client):
+    return client.get('/logout', follow_redirects=True)
 
 
 @pytest.fixture(scope='module')
 def client():
-    flask_app = create_app()
+    app = create_app()
 
     # Flask provides a way to test your application by exposing the Werkzeug test Client
     # and handling the context locals for you.
-    testing_client = flask_app.test_client()
+    client = app.test_client()
 
-    flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    db.init_app(flask_app)
-
+    app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('TEST_DATABASE_URL') \
+                                            or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
     # Establish an application context before running the tests.
-    ctx = flask_app.app_context()
-    with flask_app.app_context():
-        db.create_all()
-
+    ctx = app.app_context()
     ctx.push()
+    db.create_all()
+    u = User(username='smith', email='smith@example.com', confirmed=True)
+    u.set_password('1q2w3e')
 
-    yield testing_client  # this is where the testing happens!
+    t = Tip(body='hello there', moderated=True, user_id=1)
+    db.session.add(t)
+    db.session.add(u)
+    db.session.commit()
 
+    yield client  # this is where the testing happens!
+    db.session.remove()
     db.drop_all()
+    os.remove(os.path.join(basedir, 'data-test.sqlite'))
     ctx.pop()
 
 
-@pytest.fixture(scope='module')
-def init_database():
-    # Create the database and the database table
-    db.create_all()
-
-    # Insert user data
-    user1 = User(username='john', email='patkennedy79@gmail.com')
-    user2 = User(username='kale', email='kennedyfamilyrecipes@gmail.com')
-    db.session.add(user1)
-    db.session.add(user2)
-
-    # Commit the changes for the users
-    db.session.commit()
-
-    yield db  # this is where the testing happens!
-
-    db.drop_all()
-
-
-def test_home_page(client, init_database):
+def test_home_page(client):
     response = client.get('/tips')
+    assert b'hello there' in response.data
     assert response.status_code == 200
+
+
+def test_login_logout(client):
+    response = login(client, 'smith', '1q2w3e')
+    assert response.status_code == 200
+
+
+def test_create_post_no_user(client):
+    response = client.post('/tips/new', data=dict(
+        tip='new tip'
+    ), follow_redirects=True)
+    assert b'Please log in to access this page' in response.data
+
+
+def test_create_post_with_user(client):
+    login(client, 'smith', '1q2w3e')
+    response = client.post('/tips/new', data=dict(
+        tip='new tip',
+        hashtags=''
+    ), follow_redirects=True)
+    assert b'Your tip is now live!' in response.data
 
 
 def test_notification_empty_list(client):
